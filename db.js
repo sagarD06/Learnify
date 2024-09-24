c8c644fb2f70adc491f51eb135040ca88f4bb06f
Sagar D <83652234+sagarD06@users.noreply.github.com>
2024-09-24 19:57:37 +0530

    bug fixes

--- db.js
+++ db.js
@@ -1,7 +1,94 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 import "dotenv/config";
 
 // Connect to MongoDB
-export default function dbConnect() {
-  mongoose.connect(process.env.MONGODB_URI);
+export default async function dbConnect() {
+  await mongoose.connect(process.env.MONGODB_URI);
 }
+
+//Schemas
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const adminSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  creatorId: {
+    type: Types.ObjectId,
+    ref: "adminSchema",
+  },
+});
+
+const purchaseSchema = new mongoose.Schema({
+  userId: {
+    type: Types.ObjectId,
+    ref: "userSchema",
+  },
+  courseId: {
+    type: Types.ObjectId,
+    ref: "courseSchema",
+  },
+  purchaseDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Models
+
+export const User = mongoose.model("User", userSchema);
+export const Admin = mongoose.model("Admin", adminSchema);
+export const Course = mongoose.model("Course", courseSchema);
+export const Purchase = mongoose.model("Purchase", purchaseSchema);
--- index.js
+++ index.js
@@ -2,13 +2,16 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-import {userRouter} from "./routes/userRoutes.js";
-import {courseRouter} from "./routes/courseRoutes.js";
-import {adminRouter} from "./routes/adminRoutes.js";
+import { userRouter } from "./routes/userRoutes.js";
+import { courseRouter } from "./routes/courseRoutes.js";
+import { adminRouter } from "./routes/adminRoutes.js";
+import dbConnect from "./db.js";
 
 const app = express();
 app.use(express.json());
 const port = 3000;
+
+dbConnect(); // Connect to MongoDB database
 
 /** USERS ROUTES */
 app.use("/api/v1/user", userRouter);
