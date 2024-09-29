a15e0aedcde9b698881af4ceb0a55817f12b21a8
Sagar D <83652234+sagarD06@users.noreply.github.com>
2024-09-29 10:08:56 +0530

    Add courses route functionality

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,16 +1,45 @@
 import express from "express";
 
+import { Course, Purchase } from "../db.js";
+import userAuth from "./middleware/userMiddleware.js";
+
 const courseRouter = express.Router();
 
-courseRouter.get("/", function (req, res) {
-    res.json({
-        message : "Course"
-    })
-});
-courseRouter.post("/purchase", function (req, res) {
-    res.json({
-        message : "Course"
-    })
+/** GET ALL COURSES **/
+courseRouter.get("/", async function (req, res) {
+  try {
+    const courses = await Course.find({}).select("-creatorId");
+    res.status(200).json({
+      message: "All courses fetched succesfully",
+      success: true,
+      courses: courses,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || "something went wrong while fetching courses",
+      success: false,
+    });
+  }
 });
 
-export {courseRouter};
+/** PURCHASE COURSE **/
+courseRouter.post("/purchase/:courseId", userAuth, async function (req, res) {
+  const userId = req.userId;
+  const courseId = req.params.courseId;
+
+  try {
+    await Purchase.create({userId, courseId});
+    res.status(200).json({
+      message: "Course purchased successfully",
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message:
+        error.message || "something went wrong while purchasing the course",
+      success: false,
+    });
+  }
+});
+
+export { courseRouter };
