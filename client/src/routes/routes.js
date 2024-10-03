4fbf386a7a207f086f3b19dbd20c7a82f89d1be2
Sagar D <83652234+sagarD06@users.noreply.github.com>
2024-10-03 15:13:47 +0530

    User dashborad route initialised

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import UserSignUp from "../components/user/UserSignUp";
 import UserSignIn from "../components/user/UserSignIn";
+import UserDashboard from "../components/user/UserDashboard";
 import AdminSignUp from "../components/admin/AdminSignUp";
 import AdminSignIn from "../components/admin/AdminSignIn";
 import AdminDashboard from "../components/admin/AdminDashboard";
@@ -14,6 +15,7 @@ export const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/user/sign-up", element: <UserSignUp /> },
   { path: "/user/sign-in", element: <UserSignIn /> },
+  { path: "/user/dashboard", element: <UserDashboard /> },
   { path: "/admin/sign-up", element: <AdminSignUp /> },
   { path: "/admin/sign-in", element: <AdminSignIn /> },
   { path: "/admin/dashboard", element: <AdminDashboard /> },
